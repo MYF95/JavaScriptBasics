7a4609d901b57d598bbf015d79d713c62551e135
myf95 <myf1995@gmail.com>
2017-10-13 14:52:05 +0100

    UPDATE: index.html, myScript.js index.html	--> id=random added myScript.js 	--> function "functionLastName()" which uses same name variables with different case

--- myScript.js
+++ myScript.js
@@ -1,4 +1,12 @@
+var lastName = "Doe";
+var lastname = "Peterson";
+
 function myFunction(){
     document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 document.getElementById("demo").innerHTML = "My fasuto Dogescript";
+
+function functionLastName(){
+    document.getElementById("random").innerHTML = lastName;
+}
+document.getElementById("random").innerHTML = lastname;
